ead029a0b3d9776fc6c7857be988f59b0cf91ff4
Franco <freemidis@gmail.com>
2025-12-15 19:16:29 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
 })
 
 /** @type {import('next').NextConfig} */
